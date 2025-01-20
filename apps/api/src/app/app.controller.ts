c019fb7fff22f1f41d0ce477e3e04a5e5c970b9d
Antoine B <antoine.bayard.1@ulaval.ca>
2025-01-20 14:20:17 -0500

    init: project

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HealthResponse } from '@ugram/shared-types';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getData() {
+  getData(): HealthResponse {
     return this.appService.getData();
   }
 }
